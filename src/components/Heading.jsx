bca172a326c722cdb35e8530b9915d234cbebdc5
hanafi407 <menabung08@gmail.com>
2021-10-10 05:50:43 +0700

    Book Project

--- src/components/Heading.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-function Heading(){
-  return <h1>makanan favorit</h1>
-}
-
-export default Heading
--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 
-//code belajar
+//CSS
+import "./index.css";
 
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById("root")
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
 );
 
+const Title = () => (
+  <h1>
+    Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
+  </h1>
+);
+
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    James Clear
+  </h4>
+);
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
