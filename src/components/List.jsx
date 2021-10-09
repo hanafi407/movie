7aceafd926aa536b1dc60f24bb813618e11a97f7
hanafi407 <menabung08@gmail.com>
2021-10-09 14:25:49 +0700

    upload reactjs

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function List(){
+  return (
+  <ul>
+    <li>daging</li>
+    <li>sayur</li>
+    <li>buah</li>
+  </ul>
+  )
+              
+}
+
+export default List
