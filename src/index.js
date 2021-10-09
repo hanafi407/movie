96cb9d4d49b3526e9cedf1dd6f017277dfcbcffa
hanafi407 <menabung08@gmail.com>
2021-10-10 05:56:13 +0700

    menggabungkan komponen img, title, author ke dalam componen book

--- src/index.js
+++ src/index.js
@@ -21,30 +21,19 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
+      <h1>
+    Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
+  </h1>
+       <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    James Clear
+  </h4>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => (
-  <h1>
-    Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
-  </h1>
-);
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    James Clear
-  </h4>
-);
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
