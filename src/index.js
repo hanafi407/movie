39f3b36936df335a4ae05aee40f779a7fa059e97
hanafi407 <menabung08@gmail.com>
2021-10-10 09:20:26 +0700

    promp list

--- src/index.js
+++ src/index.js
@@ -4,35 +4,47 @@ import ReactDOM from "react-dom";
 //CSS
 import "./index.css";
 
-const book1 = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg",
-  title:
-    "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
-  author: "James Clear",
-};
-const book2 = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
-  title:
-    "The Four Agreements: A Practical Guide to Personal Freedom (A Toltec Wisdom Book)",
-  author: "Don Miguel Ruiz ",
-};
-const book3 = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71KTSD8PwLS._AC_UL200_SR200,200_.jpg",
-  title: "Taste: My Life Through Food",
-  author: "Stanley Tucci",
-};
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
+    author: "James Clear",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
+    title: "The Four Agreements: A Practical Guide to Personal Freedom",
+    author: "Don Miguel Ruiz ",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71KTSD8PwLS._AC_UL200_SR200,200_.jpg",
+    title: "Taste: My Life Through Food",
+    author: "Stanley Tucci",
+  },
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81dAl0oqP9L._AC_UL200_SR200,200_.jpg",
+    title: "The Wish",
+    author: "Nicholas Sparks",
+  },
+];
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book img={book1.img} title={book1.title} author={book1.author} />
-      <Book img={book2.img} title={book2.title} author={book2.author} />
-      <Book img={book3.img} title={book3.title} author={book3.author} />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author }) => {
-  // const {img,title,author}=props
+const Book = (props) => {
+  console.log(props)
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
