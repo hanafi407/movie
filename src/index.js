c5d40b78ae32ff3b7c3fa3859ebb22e93fb72834
hanafi407 <menabung08@gmail.com>
2021-10-10 09:54:38 +0700

    menambahkan event handling

--- src/index.js
+++ src/index.js
@@ -43,13 +43,29 @@ function BookList() {
 }
 
 const Book = (props) => {
-  console.log(props)
   const { img, title, author } = props;
+  const clickHandler = () => {
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        button
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        complex example
+      </button>
     </article>
   );
 };
