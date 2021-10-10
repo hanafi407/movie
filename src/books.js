4585a3759f9ab404359aebe9ede3a570a2b99709
hanafi407 <menabung08@gmail.com>
2021-10-10 10:21:25 +0700

    import dan export

---
+++ src/books.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
+    author: "James Clear",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
+    title: "The Four Agreements: A Practical Guide to Personal Freedom",
+    author: "Don Miguel Ruiz ",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71KTSD8PwLS._AC_UL200_SR200,200_.jpg",
+    title: "Taste: My Life Through Food",
+    author: "Stanley Tucci",
+  },
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81dAl0oqP9L._AC_UL200_SR200,200_.jpg",
+    title: "The Wish",
+    author: "Nicholas Sparks",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -4,70 +4,19 @@ import ReactDOM from "react-dom";
 //CSS
 import "./index.css";
 
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg",
-    title:
-      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
-    author: "James Clear",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
-    title: "The Four Agreements: A Practical Guide to Personal Freedom",
-    author: "Don Miguel Ruiz ",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/71KTSD8PwLS._AC_UL200_SR200,200_.jpg",
-    title: "Taste: My Life Through Food",
-    author: "Stanley Tucci",
-  },
-  {
-    id: 4,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81dAl0oqP9L._AC_UL200_SR200,200_.jpg",
-    title: "The Wish",
-    author: "Nicholas Sparks",
-  },
-];
+import{data} from './books'
+import Book from './Book'
 
 function BookList() {
   return (
     <section className="booklist">
-      {books.map((book) => {
+      {data.map((book) => {
         return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
-  const clickHandler = () => {
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        button
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        complex example
-      </button>
-    </article>
-  );
-};
+
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
